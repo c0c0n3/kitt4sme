67402be548c5fb5bec775a87c7c22e95e9a3ed43
Andrea Falconi <andrea.falconi@gmail.com>
2021-04-04 20:24:48 +0200

    fix up subs f/w service path.

--- poc/scenarios/basic-sense/2.subscribe.sh
+++ poc/scenarios/basic-sense/2.subscribe.sh
@@ -26,8 +26,27 @@ print_header "Creating catch-all ManuFracture entities subscription for QL."
 curl -v "${K4S_ORION}/v2/subscriptions" \
      -H 'Content-Type: application/json' \
      -H 'fiware-service: manufracture' \
-     -H 'fiware-servicepath: /' \
      -d @"${SCRIPTDIR}/quantumleap.subscription.json"
+
+# NOTE. FIWARE service path.
+# The way it behaves for subscriptions is a bit counter intuitive.
+# You'd expect that with a header of 'fiware-servicepath: /' Orion would
+# notify you of changes to *any* entities in the tree, similar to queries.
+# But in actual fact, to do that you'd have to omit the service path header,
+# which is what we do here. Basically the way it works is that if you
+# specify a service path, then Orion only considers entities right under
+# the last node in the service path, but not any other entities that might
+# sit further down below. E.g. if your service tree looks like (e stands
+# for entity)
+#
+#                        /
+#                     p     q
+#                  e1   r     e4
+#                     e2 e3
+#
+# then a subscription with a service path of '/' won't catch any entities
+# at all whereas one with a service path of '/p' will consider changes to
+# e1 but not e2 nor e3.
 
 print_header "Retrieving ManuFracture subscription from Context Broker."
 
@@ -40,7 +59,6 @@ print_header "Creating catch-all Smithereens entities subscription for QL."
 curl -v "${K4S_ORION}/v2/subscriptions" \
      -H 'Content-Type: application/json' \
      -H 'fiware-service: smithereens' \
-     -H 'fiware-servicepath: /' \
      -d @"${SCRIPTDIR}/quantumleap.subscription.json"
 
 print_header "Retrieving Smithereens subscription from Context Broker."
